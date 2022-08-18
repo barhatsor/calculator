040c63d68fbba78bcf18335c718e04c4d6d202d0
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-08-18 10:59:07 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,2 +1,37 @@
 
-
+let calc = {
+  el: document.querySelector('.calc'),
+  result: document.querySelector('.calc .header .number'),
+  buttons: {}
+};
+
+calc.buttons = calc.el.querySelectorAll('.buttons');
+
+calc.buttons.forEach(button => {
+    
+  if (button.classList.contains('number') ||
+      button.classList.contains('text')) {
+    
+    button.type = 'literal';
+    button.title = button.textContent;
+    
+  } else {
+    
+    button.type = 'function';
+    button.title = button.classList.replace('button', '');
+    
+  }
+  
+  
+  button.addEventListener('click', () => {
+    
+    if (button.type == 'literal') {
+      
+      result.textContent += button.title;
+      
+    }
+    
+  });
+    
+});
+
