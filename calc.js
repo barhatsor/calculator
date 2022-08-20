917369845fed60dadda8c3c2cc5f1b744ebc119a
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-08-20 13:07:31 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -804,14 +804,24 @@ document.addEventListener('keydown', (e) => {
       e.preventDefault();
       
       if (calc.history[0]) {
-    
-        if (calc.history[1]) calc.historyEl.textContent = calc.history[1];
-        else calc.historyEl.textContent = '';
-    
-        result.textContent = calc.history[0];
-        calc.result.moveSelToEnd();
-    
-        calc.history.shift();
+        
+        calc.result.classList.add('transition');
+        calc.historyEl.classList.add('transition');
+        
+        window.setTimeout(() => {
+        
+          calc.result.classList.remove('transition');
+          calc.historyEl.classList.remove('transition');
+        
+          if (calc.history[1]) calc.historyEl.textContent = calc.history[1];
+          else calc.historyEl.textContent = '';
+      
+          result.textContent = calc.history[0];
+          calc.result.moveSelToEnd();
+      
+          calc.history.shift();
+          
+        }, 180);
     
       }
     
