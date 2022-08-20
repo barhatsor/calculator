d992248cfee4069af83c957183ff92726eef8bb7
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-08-20 16:18:54 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -803,10 +803,10 @@ document.addEventListener('keydown', (e) => {
       
       e.preventDefault();
       
-      if (calc.el.classList.contains('history')) {
+      if (calc.history[0]) {
         
-        if (calc.history[0]) {
-      
+        if (calc.el.classList.contains('history')) {
+        
           calc.result.classList.add('transition');
           calc.historyEl.classList.add('transition');
         
@@ -818,7 +818,7 @@ document.addEventListener('keydown', (e) => {
             if (calc.history[1]) calc.historyEl.textContent = calc.history[1];
             else calc.historyEl.textContent = '';
         
-            result.textContent = calc.history[0];
+            calc.result.textContent = calc.history[0];
             calc.result.moveSelToEnd();
         
             calc.history.shift();
@@ -830,7 +830,7 @@ document.addEventListener('keydown', (e) => {
           if (calc.history[1]) calc.historyEl.textContent = calc.history[1];
           else calc.historyEl.textContent = '';
         
-          result.textContent = calc.history[0];
+          calc.result.textContent = calc.history[0];
           calc.result.moveSelToEnd();
         
           calc.history.shift();
@@ -891,43 +891,43 @@ document.addEventListener('paste', (e) => {
 
 
 calc.historyEl.addEventListener('click', () => {
-
-  if (calc.el.classList.contains('history')) {
-    
-    if (calc.history[0]) {
   
+  if (calc.history[0]) {
+
+    if (calc.el.classList.contains('history')) {
+
       calc.result.classList.add('transition');
       calc.historyEl.classList.add('transition');
-    
+
       window.setTimeout(() => {
-    
+
         calc.result.classList.remove('transition');
         calc.historyEl.classList.remove('transition');
-    
+
         if (calc.history[1]) calc.historyEl.textContent = calc.history[1];
         else calc.historyEl.textContent = '';
-    
-        result.textContent = calc.history[0];
+
+        calc.result.textContent = calc.history[0];
         calc.result.moveSelToEnd();
-    
+
         calc.history.shift();
-    
+
       }, 180);
-    
+
     } else {
-    
+
       if (calc.history[1]) calc.historyEl.textContent = calc.history[1];
       else calc.historyEl.textContent = '';
-    
-      result.textContent = calc.history[0];
+
+      calc.result.textContent = calc.history[0];
       calc.result.moveSelToEnd();
-    
+
       calc.history.shift();
-    
+
     }
-  
+
   }
-  
+
 });
 
 
